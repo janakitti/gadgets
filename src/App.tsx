c92af3ab442805ce0a4b0a7cff5e9bfcbb7b2397
janakitti <janakittiratana@gmail.com>
2024-01-13 17:17:49 -0500

    Add Mantine UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createTheme, MantineProvider } from "@mantine/core";
+import { Title } from "@mantine/core";
+
+const theme = createTheme({
+  fontFamily: "Poppins, Open Sans, sans-serif",
+  colors: {
+    purple: [
+      "#f4e9ff",
+      "#e3cfff",
+      "#c19bff",
+      "#9f64ff",
+      "#8236ff",
+      "#7018ff",
+      "#6707ff",
+      "#5500e5",
+      "#4c00cd",
+      "#4000b4",
+    ],
+  },
+  primaryColor: "purple",
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Title>Hello</Title>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </MantineProvider>
   );
 }
 
