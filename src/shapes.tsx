6f27c02405c0fbe9e0737351256932938d3795b8
janakitti <janakittiratana@gmail.com>
2024-01-21 12:28:49 -0500

    Add Cyclinder wrapper

---
+++ src/shapes.tsx
@@ -0,0 +1,41 @@
+import { Euler, Vector3 } from "three";
+
+interface ICylinderProps {
+  position?: Vector3;
+  rotation?: number[];
+  radiusTop?: number;
+  radiusBottom?: number;
+  height?: number;
+  radialSegments?: number;
+  isOpenEnded?: boolean;
+  color?: string;
+}
+
+export const Cylinder = ({
+  position = new Vector3(),
+  rotation = [0, 0, 0],
+  radiusTop = 1,
+  radiusBottom = 1,
+  height = 1,
+  radialSegments = 32,
+  isOpenEnded = false,
+  color = "#FFFFFF",
+}: ICylinderProps) => {
+  return (
+    <group scale={1} position={position} rotation={new Euler(...rotation)}>
+      <mesh>
+        <cylinderGeometry
+          args={[
+            radiusTop,
+            radiusBottom,
+            height,
+            radialSegments,
+            radialSegments,
+            isOpenEnded,
+          ]}
+        />
+        <meshStandardMaterial color={color} />
+      </mesh>
+    </group>
+  );
+};
